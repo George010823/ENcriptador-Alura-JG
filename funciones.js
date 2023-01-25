e3e661b6b85db4587eeaa43fb278150b6ed33f81
George010823 <jorgomezre@unal.edu.co>
2023-01-25 13:12:02 -0500

    Cambiando if por arrays en la encriptacion

--- funciones.js
+++ funciones.js
@@ -2,29 +2,36 @@ function init()
 {
     document.getElementById("txt_input").focus();
 }
-
-var cadenaTextoLimpio;          
+  
+const cadenaTextoLimpio = document.querySelector("#txt_input");        
+const mensaje = document.querySelector("#txt_output");        
 var retorno = false;
 
-function encriptarTexto() {
-    init();
-    var arrayCadenaEncriptar = [];
-    cadenaTextoLimpio = document.getElementById("txt_input").value;
+function btnEncriptar() {
+    const textoEncriptado = encriptarTexto(cadenaTextoLimpio.value)
+    cadenaTextoLimpio.value = "";
+}
 
+function btnDesencriptar() {
+    const textoEncriptado = desencriptarTexto(cadenaTextoLimpio.value)
+    cadenaTextoLimpio.value = "";
+}
+
+function encriptarTexto(cadenaTextoLimpio) {
     if(cadenaTextoLimpio.trim() === ""){
         alert("NO HA ESCRITO UN MENSAJE PARA CIFRAR.")
+        init();
     }else{
         cadenaTextoLimpio = cadenaTextoLimpio.toLowerCase();
-        arrayCadenaEncriptar = cadenaTextoLimpio.split("");
 
-        validarCaracteresEspeciales(arrayCadenaEncriptar);
+        validarCaracteresEspeciales(cadenaTextoLimpio);
 
         if(retorno){
             document.getElementById("presentacion_inicial").style.display = "none";
             document.getElementById("txt_output").style.display = "block";
             document.getElementById("btnCopiar").style.display = "block";
 
-            fnEncriptarTexto(arrayCadenaEncriptar);
+            fnEncriptarTexto(cadenaTextoLimpio);
         }
     }
 }
@@ -46,35 +53,21 @@ function validarCaracteresEspeciales(caracteres){
 }
 
 function fnEncriptarTexto(cadena){
-        for(i = 0; i < cadena.length; i ++){
-            if(cadena[i] == 'a'){
-                cadena.splice(i, 1, 'ai');
-            }else if(cadena[i] == 'e'){
-                cadena.splice(i, 1, 'enter');
-            }else if(cadena[i] == 'i'){
-                cadena.splice(i, 1, 'imes');
-            }else if(cadena[i] == 'o'){
-                cadena.splice(i, 1, 'ober');
-            }else if(cadena[i] == 'u'){
-                cadena.splice(i, 1, 'ufat');
+    let arrayCadenaEncriptar = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']];
+
+        for(let i = 0; i < arrayCadenaEncriptar.length; i++){
+            if(cadena.includes(arrayCadenaEncriptar[i][0])){
+                cadena = cadena.replaceAll(arrayCadenaEncriptar[i][0], arrayCadenaEncriptar[i][1]);
             }
         }
-        mostrarTextoEncriptado(cadena)
-}
+        
+        mensaje.value = cadena;
+}   
 
-function mostrarTextoEncriptado(cadena){
-    cadena = cadena.toString();
-
-    let textoArea = document.querySelector("#txt_output")
-    textoArea.innerText = cadena.replace(/,/g, "");
-}
-
-function desencriptarTexto(){
-    init();
-    cadenaTextoLimpio = document.getElementById("txt_input").value;
-
+function desencriptarTexto(cadenaTextoLimpio){
     if(cadenaTextoLimpio.trim() === ""){
         alert("NO HA ESCRITO UN MENSAJE PARA DESCIFRAR.")
+        init();
     }else{    
         cadenaTextoLimpio = cadenaTextoLimpio.toLowerCase();
         fnDesencriptarTexto(cadenaTextoLimpio);
@@ -82,49 +75,27 @@ function desencriptarTexto(){
 }
 
 function fnDesencriptarTexto(cadena){
-    var bandera = false;
+    let arrayCadenaEncriptar = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']];
+    let bandera = false;
 
-    if(cadena.includes('ai')){
-        bandera = true;
-        const re = /ai/gi;
-        cadena = cadena.replace(re, 'a'); 
+    for(let i = 0; i < arrayCadenaEncriptar.length; i++){
+        if(cadena.includes(arrayCadenaEncriptar[i][1])){
+            cadena = cadena.replaceAll(arrayCadenaEncriptar[i][1], arrayCadenaEncriptar[i][0]);
+            bandera = true;
+        }
     }
-    if(cadena.includes('enter')){
-        bandera = true;
-        const re = /enter/gi;
-        cadena = cadena.replace(re, 'e');
-    }
-    if(cadena.includes('imes')){
-        bandera = true;
-        const re = /imes/gi;
-        cadena = cadena.replace(re, 'i');
-    }
-    if(cadena.includes('ober')){
-        bandera = true;
-        const re = /ober/gi;
-        cadena = cadena.replace(re, 'o');
-    }
-    if(cadena.includes('ufat')){
-        bandera = true;
-        const re = /ufat/gi;
-        cadena = cadena.replace(re, 'u');
-    } 
-
+        
     if(bandera){
+        init();
         document.getElementById("presentacion_inicial").style.display = "none";
         document.getElementById("txt_output").style.display = "block";
         document.getElementById("btnCopiar").style.display = "block";
         
-        mostrarTextoDesencriptado(cadena);
+        mensaje.value = cadena;
     }else{
         alert("EL MENSAJE NO CONTIENE PARAMETROS DE DESENCRIPCION.")
+        init();
     }
-}
-
-function mostrarTextoDesencriptado(cadena){
-
-    let textoArea = document.querySelector("#txt_output")
-    textoArea.innerText = cadena;
 }
 
 document.getElementById("btnCopiar").onclick = function() {
